27573aeb2524a0878de912b4dac7e0fd8dfa0206
sdoumbouya <sdoumbouya@pinterest.com>
2021-08-11 13:24:30 -0700

    Adds vpc_endpoint_sservices

--- providers/aws/vpc_endpoints.go
+++ providers/aws/vpc_endpoints.go
@@ -16,6 +16,7 @@ package aws
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/GoogleCloudPlatform/terraformer/terraformutils"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -38,6 +39,9 @@ func (g *VpcEndpointGenerator) InitResources() error {
 	if err := g.loadEndpoints(svc); err != nil {
 		return err
 	}
+	if err := g.loadServices(svc); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -53,6 +57,42 @@ func (g *VpcEndpointGenerator) loadEndpoints(svc *ec2.Client) error {
 				StringValue(vpcEndpoint.VpcEndpointId),
 				StringValue(vpcEndpoint.VpcEndpointId),
 				"aws_vpc_endpoint",
+				"aws",
+				endpointAllowEmptyValues,
+			))
+		}
+	}
+
+	return nil
+}
+
+// PostConvertHook for add policy json as heredoc
+func (g *VpcEndpointGenerator) PostConvertHook() error {
+	for i, resource := range g.Resources {
+		if resource.InstanceInfo.Type == "aws_vpc_endpoint" {
+			if val, ok := g.Resources[i].Item["policy"]; ok {
+				policy := g.escapeAwsInterpolation(val.(string))
+				g.Resources[i].Item["policy"] = fmt.Sprintf(`<<POLICY
+%s
+POLICY`, policy)
+			}
+		}
+	}
+	return nil
+}
+
+func (g *VpcEndpointGenerator) loadServices(svc *ec2.Client) error {
+	p := ec2.NewDescribeVpcEndpointServiceConfigurationsPaginator(svc, &ec2.DescribeVpcEndpointServiceConfigurationsInput{})
+	for p.HasMorePages() {
+		page, err := p.NextPage(context.TODO())
+		if err != nil {
+			return err
+		}
+		for _, ServiceConfiguration := range page.ServiceConfigurations {
+			g.Resources = append(g.Resources, terraformutils.NewSimpleResource(
+				StringValue(ServiceConfiguration.ServiceId),
+				StringValue(ServiceConfiguration.ServiceId),
+				"aws_vpc_endpoint_service",
 				"aws",
 				endpointAllowEmptyValues,
 			))
